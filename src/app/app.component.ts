00e98e6aee98e49733d505ea1d17bef3234a0688
“neyser-eviden” <neyser.huaman.external@eviden.com>
2024-04-25 12:33:27 +0200

    Uso de For

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,20 @@ export class AppComponent {
   titulo: string = 'Registro de Usuarios';
   mensaje: string = "";
   registrado: boolean = false;
-
-  nombre: string = "";
+  nombre: string = "";  
   apellido: string = "";
+  entradas: Array<{titulo:string}>;
+
+  constructor(){
+    this.entradas=[
+      {titulo:"Python cada día más presente"},
+      {titulo:"Java presente hace más de 20 años"},
+      {titulo:"Javascript Cada vez más funcional"},
+      {titulo:"Kotlin Es Android Renovado"},
+      {titulo:"¿Dónde quedó Pascas?"}
+      
+    ]
+  }
 
   registrarUsuario(){
     this.registrado = true;
